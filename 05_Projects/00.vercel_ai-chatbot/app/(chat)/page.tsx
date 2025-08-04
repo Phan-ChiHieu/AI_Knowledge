69b39119d42ede2637efb815edb29ee06405bf29
EdricPhan <edricphan.dev@gmail.com>
2025-08-04 22:40:57 +0700

    update code 00.vercel_ai_chatbot

---
+++ 05_Projects/00.vercel_ai-chatbot/app/(chat)/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className="text-red-800">Page Home</div>;
+}
---
+++ 05_Projects/00.vercel_ai-chatbot/app/layout.tsx
@@ -0,0 +1,41 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+export const viewport = {
+  maximumScale: 1, // Disable auto-zoom on mobile Safari
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-geist",
+});
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-geist-mono",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      // `next-themes` injects an extra classname to the body element to avoid
+      // visual flicker before hydration. Hence the `suppressHydrationWarning`
+      // prop is necessary to avoid the React hydration mismatch warning.
+      // https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
+      suppressHydrationWarning
+      className={`${geist.variable} ${geistMono.variable}`}
+    >
+      <head></head>
+      <body className="antialiased">
+        <>{children}</>
+      </body>
+    </html>
+  );
+}
